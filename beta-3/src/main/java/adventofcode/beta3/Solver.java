23a9327f0d26be99f004bcaeea3a648b616809e3
Jason Tan <j.tan@obscuritylabs.com>
2023-11-21 11:16:22 -0500

    solve: add Java solution for 2022.3.1

--- beta-3/src/main/java/adventofcode/beta3/Solver.java
+++ beta-3/src/main/java/adventofcode/beta3/Solver.java
@@ -1,10 +1,37 @@
 package adventofcode.beta3;
 
+import java.lang.IllegalArgumentException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /** Solves the Advent of Code problem.
  */
 public class Solver {
 
-  public int add(int a, int b) {
-    return a + b;
+  public static String[] parseData(String input) {
+    int size = input.length() / 2;
+    String first = input.substring(0, size);
+    String second = input.substring(size, input.length());
+
+    return new String[]{first, second};
+  }
+
+  public static String findOdd(String first, String second) {
+    Set<String> firstRucksack = new HashSet<String>(Arrays.asList(first.split("")));
+    Set<String> secondRucksack = new HashSet<String>(Arrays.asList(second.split("")));
+
+    firstRucksack.retainAll(secondRucksack);
+    return firstRucksack.toArray(new String[0])[0];
+  }
+
+  public static int calculatePriority(char value) {
+    String map = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int priority = map.indexOf(value);
+    if (priority == -1) {
+      throw new IllegalArgumentException("value is not in alphabet");
+    } else {
+      return priority + 1;
+    }
   }
 }
--- beta-3/src/test/java/adventofcode/beta3/SolverTests.java
+++ beta-3/src/test/java/adventofcode/beta3/SolverTests.java
@@ -1,16 +1,50 @@
 package adventofcode.beta3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class SolverTests {
 
-  @Test
-  @DisplayName("1 + 1 = 2")
-  void addsTwoNumbers() {
-    Solver solver = new Solver();
-    assertEquals(2, solver.add(1, 1), "1 + 1 should equal 2");
+  @ParameterizedTest
+  @CsvSource({
+    "vJrwpWtwJgWrhcsFMMfFFhFp, vJrwpWtwJgWr, hcsFMMfFFhFp",
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL, jqHRNqRjqzjGDLGL, rsFMfFZSrLrFZsSL",
+    "PmmdzqPrVvPwwTWBwg, PmmdzqPrV, vPwwTWBwg",
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn, wMqvLMZHhHMvwLH, jbvcjnnSBnvTQFn",
+    "ttgJtRGJQctTZtZT, ttgJtRGJ, QctTZtZT",
+    "CrZsJsPPZsGzwwsLwLmpwMDw, CrZsJsPPZsGz, wwsLwLmpwMDw",
+  })
+  void parsesData(String input, String rucksack1, String rucksack2) {
+    String[] expected = new String[]{rucksack1, rucksack2};
+    assertArrayEquals(expected, Solver.parseData(input));
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+    "vJrwpWtwJgWr, hcsFMMfFFhFp, p",
+    "jqHRNqRjqzjGDLGL, rsFMfFZSrLrFZsSL, L",
+    "PmmdzqPrV, vPwwTWBwg, P",
+    "wMqvLMZHhHMvwLH, jbvcjnnSBnvTQFn, v",
+    "ttgJtRGJ, QctTZtZT, t",
+    "CrZsJsPPZsGz, wwsLwLmpwMDw, s",
+  })
+  void findsOddOne(String in1, String in2, String odd) {
+    assertEquals(odd, Solver.findOdd(in1, in2));
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+    "p, 16",
+    "L, 38",
+    "P, 42",
+    "v, 22",
+    "t, 20",
+    "s, 19",
+  })
+  void convertToPriority(char value, int priority) {
+    assertEquals(Solver.calculatePriority(value), priority);
   }
 }
