cfdf3b6b947561b8cae45cea93e986c43d625251
Jason Tan <j.tan@obscuritylabs.com>
2023-11-22 17:52:59 -0500

    refactor: combine expected test results into one

--- beta-6/tests/test_solve.py
+++ beta-6/tests/test_solve.py
@@ -2,29 +2,20 @@ from solve import solve
 
 import pytest
 
-packetdata = [
-    ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
-    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-    ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+testdata = [
+    ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 19),
+    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23),
+    ("nppdvjthqldpwncqszvftbrmjlhg", 6, 23),
+    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 29),
+    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 26),
 ]
 
 
-@pytest.mark.parametrize("stream,expected", packetdata)
-def test_detect_packet_start(stream: str, expected: int):
+@pytest.mark.parametrize("stream,expected,_", testdata)
+def test_detect_packet_start(stream: str, expected: int, _):
     assert solve.detect_start(stream, 4) == expected
 
 
-messagedata = [
-    ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
-    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
-    ("nppdvjthqldpwncqszvftbrmjlhg", 23),
-    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
-    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
-]
-
-
-@pytest.mark.parametrize("stream,expected", messagedata)
-def test_detect_message_start(stream: str, expected: int):
+@pytest.mark.parametrize("stream,_,expected", testdata)
+def test_detect_message_start(stream: str, _, expected: int):
     assert solve.detect_start(stream, 14) == expected
