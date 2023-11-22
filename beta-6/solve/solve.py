b0d1977f4f0a06555812c39032688656d3b0f221
Jason Tan <j.tan@obscuritylabs.com>
2023-11-22 17:45:06 -0500

    solve: add Python solution for 2022.6.1

--- beta-6/solve/solve.py
+++ beta-6/solve/solve.py
@@ -1,10 +1,28 @@
 def func(x):
     return x + 1
 
+def are_all_unique(string: str) -> bool:
+    return len(string) == len(set(string))
+
+def detect_packet_start(stream: str) -> int:
+    index = 4
+    candidate = stream[:4]
+    remaining = stream[4:]
+
+    while not are_all_unique(candidate):
+        new_token, remaining = remaining[:1], remaining[1:]
+        candidate = candidate[1:] + new_token
+        index += 1
+
+    return index
+
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser("beta-6 solver")
     parser.add_argument("input", help="Path to the file containing puzzle input")
     args = parser.parse_args()
-    print(args.input)
+
+    with open(args.input) as f:
+        for line in f:
+            print(detect_packet_start(line))
--- beta-6/tests/test_solve.py
+++ beta-6/tests/test_solve.py
@@ -1,5 +1,15 @@
 from solve import solve
 
+import pytest
 
-def test_func():
-    assert solve.func(3) == 5
+testdata = [
+    ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
+    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+    ("nppdvjthqldpwncqszvftbrmjlhg", 6),
+    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+]
+
+@pytest.mark.parametrize("stream,expected", testdata)
+def test_detect_packet_start(stream: str, expected: int):
+    assert solve.detect_packet_start(stream) == expected
