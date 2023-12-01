012e00d399e131ce68f5d4fa53de3ecdb880e021
Jason Tan <j.tan@obscuritylabs.com>
2023-12-01 12:16:50 -0500

    feat: read letters too when extracting digits

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,43 +1,94 @@
 use clap::Parser;
 use std::{error::Error, fs::read_to_string};
 
-fn extract_digits(calibration: String) -> Vec<i32>
-{
-    calibration.chars()
-        .filter(|c| c.is_ascii_digit())
-        .map(|c| c.to_digit(10))
-        .flatten()
-        .map(|u| u as i32)
-        .collect()
+fn extract_digits(calibration: String, read_letters: bool) -> Vec<i32> {
+    let mut tokens: Vec<i32> = Vec::new();
+
+    let mut chars = calibration.as_str();
+    while !chars.is_empty() {
+        let next_token: Option<i32> = match chars {
+            num if num.starts_with(|c: char| c.is_ascii_digit()) => {
+                let digit = chars[0..1].parse::<i32>().ok();
+                chars = &chars[1..];
+                digit
+            }
+            num if read_letters && num.starts_with("one")   => {
+                chars = &chars[3..];
+                Some(1)
+            }
+            num if read_letters && num.starts_with("two")   => {
+                chars = &chars[3..];
+                Some(2)
+            }
+            num if read_letters && num.starts_with("three") => {
+                chars = &chars[5..];
+                Some(3)
+            }
+            num if read_letters && num.starts_with("four")  => {
+                chars = &chars[4..];
+                Some(4)
+            }
+            num if read_letters && num.starts_with("five")  => {
+                chars = &chars[4..];
+                Some(5)
+            }
+            num if read_letters && num.starts_with("six")   => {
+                chars = &chars[3..];
+                Some(6)
+            }
+            num if read_letters && num.starts_with("seven") => {
+                chars = &chars[5..];
+                Some(7)
+            }
+            num if read_letters && num.starts_with("eight") => {
+                chars = &chars[5..];
+                Some(8)
+            }
+            num if read_letters && num.starts_with("nine")  => {
+                chars = &chars[4..];
+                Some(9)
+            }
+            _   => {
+                chars = &chars[1..];
+                None
+            }
+        };
+        match next_token {
+            Some(token) => tokens.push(token),
+            None        => (),
+        };
+    }
+
+    tokens
 }
 
-fn answer(input: String) -> i32 {
-    let digits = extract_digits(input);
+fn answer(input: String, read_letters: bool) -> i32 {
+    let digits = extract_digits(input, read_letters);
     match (digits.first(), digits.last()) {
-        (Some(x), Some(y)) => x * 10 + y,
+        (Some(x), Some(y)) => {println!("{}", x * 10 + y); x * 10 + y},
         _ => panic!("Could not retrieve desired digits.")
     }
 }
 
-fn aggregate<I>(contents: I) -> i32
+fn aggregate<I>(contents: I, read_letters: bool) -> i32
 where
     I: Iterator<Item = String>
 {
-    contents.map(|calibration| answer(calibration)).sum()
+    contents.map(|calibration| answer(calibration, read_letters)).sum()
 }
 
 #[derive(Debug, Parser)]
 struct Cli {
     file: std::path::PathBuf,
-    #[arg(default_value = None)]
-    length: Option<usize>,
+    #[arg(short, long)]
+    letters: bool,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
     let content = read_to_string(&args.file).unwrap();
     let calibrations: Vec<String> = content.lines().map(|line| line.to_string()).collect();
-    let result = aggregate(calibrations.into_iter());
+    let result = aggregate(calibrations.into_iter(), args.letters);
     println!("{:?}", result);
     Ok(())
 }
@@ -48,18 +99,34 @@ mod tests {
 
     #[test]
     fn digits() {
-        assert_eq!(extract_digits("1abc2".to_string()), vec![1, 2]);
-        assert_eq!(extract_digits("pqr3stu8vwx".to_string()), vec![3, 8]);
-        assert_eq!(extract_digits("a1b2c3d4e5f".to_string()), vec![1, 2, 3, 4, 5]);
-        assert_eq!(extract_digits("treb7uchet".to_string()), vec![7]);
+        assert_eq!(extract_digits("1abc2".to_string(), false), vec![1, 2]);
+        assert_eq!(extract_digits("pqr3stu8vwx".to_string(), false), vec![3, 8]);
+        assert_eq!(extract_digits("a1b2c3d4e5f".to_string(), false), vec![1, 2, 3, 4, 5]);
+        assert_eq!(extract_digits("treb7uchet".to_string(), false), vec![7]);
+
+        assert_eq!(extract_digits("two1nine".to_string(), true), vec![2, 1, 9]);
+        assert_eq!(extract_digits("eighttwothree".to_string(), true), vec![8, 2, 3]);
+        assert_eq!(extract_digits("abcone2threexyz".to_string(), true), vec![1, 2, 3]);
+        assert_eq!(extract_digits("xtwone3four".to_string(), true), vec![2, 3, 4]);
+        assert_eq!(extract_digits("4nineeightseven2".to_string(), true), vec![4, 9, 8, 7, 2]);
+        assert_eq!(extract_digits("zoneight234".to_string(), true), vec![1, 2, 3, 4]);
+        assert_eq!(extract_digits("7pqrstsixteen".to_string(), true), vec![7, 6]);
     }
 
     #[test]
     fn sample() {
-        assert_eq!(answer("1abc2".to_string()), 12);
-        assert_eq!(answer("pqr3stu8vwx".to_string()), 38);
-        assert_eq!(answer("a1b2c3d4e5f".to_string()), 15);
-        assert_eq!(answer("treb7uchet".to_string()), 77);
+        assert_eq!(answer("1abc2".to_string(), false), 12);
+        assert_eq!(answer("pqr3stu8vwx".to_string(), false), 38);
+        assert_eq!(answer("a1b2c3d4e5f".to_string(), false), 15);
+        assert_eq!(answer("treb7uchet".to_string(), false), 77);
+
+        assert_eq!(answer("two1nine".to_string(), true), 29);
+        assert_eq!(answer("eighttwothree".to_string(), true), 83);
+        assert_eq!(answer("abcone2threexyz".to_string(), true), 13);
+        assert_eq!(answer("xtwone3four".to_string(), true), 24);
+        assert_eq!(answer("4nineeightseven2".to_string(), true), 42);
+        assert_eq!(answer("zoneight234".to_string(), true), 14);
+        assert_eq!(answer("7pqrstsixteen".to_string(), true), 76);
     }
 
     #[test]
@@ -70,6 +137,17 @@ mod tests {
             "a1b2c3d4e5f".to_string(),
             "treb7uchet".to_string()
         ];
-        assert_eq!(aggregate(input.into_iter()), 142);
+        assert_eq!(aggregate(input.into_iter(), false), 142);
+
+        let input = vec![
+            "two1nine".to_string(),
+            "eighttwothree".to_string(),
+            "abcone2threexyz".to_string(),
+            "xtwone3four".to_string(),
+            "4nineeightseven2".to_string(),
+            "zoneight234".to_string(),
+            "7pqrstsixteen".to_string(),
+        ];
+        assert_eq!(aggregate(input.into_iter(), true), 281);
     }
 }
