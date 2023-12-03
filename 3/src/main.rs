d25703335bfaabe996b2dadd8cdccf9a70186e18
Jason Tan <j.tan@obscuritylabs.com>
2023-12-03 00:52:52 -0500

    chore: set up skeleton and tests for 2023.3.1

---
+++ 3/src/main.rs
@@ -0,0 +1,418 @@
+use clap::Parser;
+use std::{error::Error, fs::read_to_string};
+
+#[derive(Clone, Debug, PartialEq)]
+enum Content {
+    Symbol(char),
+    Number(u32),
+}
+#[derive(Clone, Debug, PartialEq)]
+struct Position {
+    row: u32,
+    columns: Vec<u32>,
+}
+#[derive(Clone, Debug, PartialEq)]
+struct Artifact {
+    content: Content,
+    position: Position,
+}
+
+fn parse_row(row_number: u32, row_data: &str) -> Vec<Artifact> {
+    vec![]
+}
+
+fn get_symbols(all: &Vec<Artifact>) -> &Vec<Artifact> {
+    all
+}
+
+fn get_parts_adjacent_to<'a>(all: &'a Vec<Artifact>, pos: &'a Position) -> &'a Vec<Artifact> {
+    all
+}
+
+fn aggregate<'a, I>(contents: I) -> u32
+where
+    I: Iterator<Item = &'a str>
+{
+    let all = contents.enumerate().map(|(i, line)| parse_row(i.try_into().unwrap(), line)).flatten().collect();
+    let mut parts: Vec<Artifact> = vec![];
+    for symbol in get_symbols(&all) {
+        parts.extend_from_slice(get_parts_adjacent_to(&all, &symbol.position));
+    }
+
+    let mut sum = 0;
+    for part in parts {
+        if let Content::Number(part_number) = part.content {
+            sum += part_number
+        }
+    }
+    sum
+}
+
+#[derive(Debug, Parser)]
+struct Cli {
+    file: std::path::PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+    let content = read_to_string(&args.file).unwrap();
+    let result = aggregate(content.lines());
+    println!("{:?}", result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_row() {
+        assert_eq!(
+            parse_row(0, "467..114.."),
+            vec![
+                Artifact {
+                    content: Content::Number(467),
+                    position: Position { row: 0, columns: vec![0, 1, 2] },
+                },
+                Artifact {
+                    content: Content::Number(114),
+                    position: Position { row: 0, columns: vec![5, 6, 7] },
+                },
+            ]
+        );
+        assert_eq!(
+            parse_row(1, "...*......"),
+            vec![
+                Artifact {
+                    content: Content::Symbol('*'),
+                    position: Position { row: 1, columns: vec![3] },
+                },
+            ]
+        );
+        assert_eq!(
+            parse_row(2, "..35..633."),
+            vec![
+                Artifact {
+                    content: Content::Number(35),
+                    position: Position { row: 2, columns: vec![2, 3] },
+                },
+                Artifact {
+                    content: Content::Number(633),
+                    position: Position { row: 2, columns: vec![6, 7, 8] },
+                },
+            ]
+        );
+        assert_eq!(
+            parse_row(3, "......#..."),
+            vec![
+                Artifact {
+                    content: Content::Symbol('#'),
+                    position: Position { row: 3, columns: vec![6] },
+                },
+            ]
+        );
+        assert_eq!(
+            parse_row(4, "617*......"),
+            vec![
+                Artifact {
+                    content: Content::Number(617),
+                    position: Position { row: 4, columns: vec![0, 1, 2] },
+                },
+                Artifact {
+                    content: Content::Symbol('*'),
+                    position: Position { row: 4, columns: vec![3] },
+                },
+            ]
+        );
+        assert_eq!(
+            parse_row(5, ".....+.58."),
+            vec![
+                Artifact {
+                    content: Content::Symbol('+'),
+                    position: Position { row: 5, columns: vec![5] },
+                },
+                Artifact {
+                    content: Content::Number(58),
+                    position: Position { row: 5, columns: vec![7, 8] },
+                },
+            ]
+        );
+        assert_eq!(
+            parse_row(6, "..592....."),
+            vec![
+                Artifact {
+                    content: Content::Number(592),
+                    position: Position { row: 6, columns: vec![2, 3, 4] },
+                },
+            ]
+        );
+        assert_eq!(
+            parse_row(7, "...$.*...."),
+            vec![
+                Artifact {
+                    content: Content::Symbol('$'),
+                    position: Position { row: 7, columns: vec![3] },
+                },
+                Artifact {
+                    content: Content::Symbol('*'),
+                    position: Position { row: 7, columns: vec![5] },
+                },
+            ]
+        );
+        assert_eq!(
+            parse_row(8, ".664.598.."),
+            vec![
+                Artifact {
+                    content: Content::Number(664),
+                    position: Position { row: 8, columns: vec![1, 2, 3] },
+                },
+                Artifact {
+                    content: Content::Number(598),
+                    position: Position { row: 8, columns: vec![5, 6, 7] },
+                },
+            ]
+        );
+    }
+
+    #[test]
+    fn test_get_symbols() {
+        let all = vec![
+            Artifact {
+                content: Content::Number(467),
+                position: Position { row: 0, columns: vec![0, 1, 2] },
+            },
+            Artifact {
+                content: Content::Number(114),
+                position: Position { row: 0, columns: vec![5, 6, 7] },
+            },
+            Artifact {
+                content: Content::Symbol('*'),
+                position: Position { row: 1, columns: vec![3] },
+            },
+            Artifact {
+                content: Content::Number(35),
+                position: Position { row: 2, columns: vec![2, 3] },
+            },
+            Artifact {
+                content: Content::Number(633),
+                position: Position { row: 2, columns: vec![6, 7, 8] },
+            },
+            Artifact {
+                content: Content::Symbol('#'),
+                position: Position { row: 3, columns: vec![6] },
+            },
+            Artifact {
+                content: Content::Number(617),
+                position: Position { row: 4, columns: vec![0, 1, 2] },
+            },
+            Artifact {
+                content: Content::Symbol('*'),
+                position: Position { row: 4, columns: vec![3] },
+            },
+            Artifact {
+                content: Content::Symbol('+'),
+                position: Position { row: 5, columns: vec![5] },
+            },
+            Artifact {
+                content: Content::Number(58),
+                position: Position { row: 5, columns: vec![7, 8] },
+            },
+            Artifact {
+                content: Content::Number(592),
+                position: Position { row: 6, columns: vec![2, 3, 4] },
+            },
+            Artifact {
+                content: Content::Symbol('$'),
+                position: Position { row: 7, columns: vec![3] },
+            },
+            Artifact {
+                content: Content::Symbol('*'),
+                position: Position { row: 7, columns: vec![5] },
+            },
+            Artifact {
+                content: Content::Number(664),
+                position: Position { row: 8, columns: vec![1, 2, 3] },
+            },
+            Artifact {
+                content: Content::Number(598),
+                position: Position { row: 8, columns: vec![5, 6, 7] },
+            },
+        ];
+        let symbols = vec![
+            Artifact {
+                content: Content::Symbol('*'),
+                position: Position { row: 1, columns: vec![3] },
+            },
+            Artifact {
+                content: Content::Symbol('#'),
+                position: Position { row: 3, columns: vec![6] },
+            },
+            Artifact {
+                content: Content::Symbol('*'),
+                position: Position { row: 4, columns: vec![3] },
+            },
+            Artifact {
+                content: Content::Symbol('+'),
+                position: Position { row: 5, columns: vec![5] },
+            },
+            Artifact {
+                content: Content::Symbol('$'),
+                position: Position { row: 7, columns: vec![3] },
+            },
+            Artifact {
+                content: Content::Symbol('*'),
+                position: Position { row: 7, columns: vec![5] },
+            },
+        ];
+        assert_eq!(get_symbols(&all), &symbols);
+    }
+
+    #[test]
+    fn test_get_parts_adjacent_to() {
+        let all = vec![
+            Artifact {
+                content: Content::Number(467),
+                position: Position { row: 0, columns: vec![0, 1, 2] },
+            },
+            Artifact {
+                content: Content::Number(114),
+                position: Position { row: 0, columns: vec![5, 6, 7] },
+            },
+            Artifact {
+                content: Content::Symbol('*'),
+                position: Position { row: 1, columns: vec![3] },
+            },
+            Artifact {
+                content: Content::Number(35),
+                position: Position { row: 2, columns: vec![2, 3] },
+            },
+            Artifact {
+                content: Content::Number(633),
+                position: Position { row: 2, columns: vec![6, 7, 8] },
+            },
+            Artifact {
+                content: Content::Symbol('#'),
+                position: Position { row: 3, columns: vec![6] },
+            },
+            Artifact {
+                content: Content::Number(617),
+                position: Position { row: 4, columns: vec![0, 1, 2] },
+            },
+            Artifact {
+                content: Content::Symbol('*'),
+                position: Position { row: 4, columns: vec![3] },
+            },
+            Artifact {
+                content: Content::Symbol('+'),
+                position: Position { row: 5, columns: vec![5] },
+            },
+            Artifact {
+                content: Content::Number(58),
+                position: Position { row: 5, columns: vec![7, 8] },
+            },
+            Artifact {
+                content: Content::Number(592),
+                position: Position { row: 6, columns: vec![2, 3, 4] },
+            },
+            Artifact {
+                content: Content::Symbol('$'),
+                position: Position { row: 7, columns: vec![3] },
+            },
+            Artifact {
+                content: Content::Symbol('*'),
+                position: Position { row: 7, columns: vec![5] },
+            },
+            Artifact {
+                content: Content::Number(664),
+                position: Position { row: 8, columns: vec![1, 2, 3] },
+            },
+            Artifact {
+                content: Content::Number(598),
+                position: Position { row: 8, columns: vec![5, 6, 7] },
+            },
+        ];
+        assert_eq!(
+            get_parts_adjacent_to(&all, &Position { row: 1, columns: vec![3] }),
+            &vec![
+                Artifact {
+                    content: Content::Number(467),
+                    position: Position { row: 0, columns: vec![0, 1, 2] },
+                },
+                Artifact {
+                    content: Content::Number(35),
+                    position: Position { row: 2, columns: vec![2, 3] },
+                },
+            ]
+        );
+        assert_eq!(
+            get_parts_adjacent_to(&all, &Position { row: 3, columns: vec![6] }),
+            &vec![
+                Artifact {
+                    content: Content::Number(633),
+                    position: Position { row: 2, columns: vec![6, 7, 8] },
+                },
+            ]
+        );
+        assert_eq!(
+            get_parts_adjacent_to(&all, &Position { row: 4, columns: vec![3] }),
+            &vec![
+                Artifact {
+                    content: Content::Number(617),
+                    position: Position { row: 4, columns: vec![0, 1, 2] },
+                },
+            ]
+        );
+        assert_eq!(
+            get_parts_adjacent_to(&all, &Position { row: 5, columns: vec![5] }),
+            &vec![
+                Artifact {
+                    content: Content::Number(592),
+                    position: Position { row: 6, columns: vec![2, 3, 4] },
+                },
+            ]
+        );
+        assert_eq!(
+            get_parts_adjacent_to(&all, &Position { row: 7, columns: vec![3] }),
+            &vec![
+                Artifact {
+                    content: Content::Number(592),
+                    position: Position { row: 6, columns: vec![2, 3, 4] },
+                },
+                Artifact {
+                    content: Content::Number(664),
+                    position: Position { row: 8, columns: vec![1, 2, 3] },
+                },
+            ]
+        );
+        assert_eq!(
+            get_parts_adjacent_to(&all, &Position { row: 7, columns: vec![5] }),
+            &vec![
+                Artifact {
+                    content: Content::Number(592),
+                    position: Position { row: 6, columns: vec![2, 3, 4] },
+                },
+                Artifact {
+                    content: Content::Number(598),
+                    position: Position { row: 8, columns: vec![5, 6, 7] },
+                },
+            ]
+        );
+    }
+
+    #[test]
+    fn test_aggregate() {
+        let input = vec![
+            "467..114..",
+            "...*......",
+            "..35..633.",
+            "......#...",
+            "617*......",
+            ".....+.58.",
+            "..592.....",
+            "......755.",
+            "...$.*....",
+            ".664.598..",
+        ];
+        assert_eq!(aggregate(input.into_iter()), 4361);
+    }
+}
